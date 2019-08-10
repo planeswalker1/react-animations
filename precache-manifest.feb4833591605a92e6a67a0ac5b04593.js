496c41e2f5ffdbd5796223f6c1a8ed109b8be12a
Daniel Munoz <daniel.munozdev@gmail.com>
2019-08-09 21:30:00 -0700

    Updates

---
+++ precache-manifest.feb4833591605a92e6a67a0ac5b04593.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31413470b21091a7ff67bb1b95ca5e4d",
+    "url": "/react-animations/index.html"
+  },
+  {
+    "revision": "2527e0d9de1b92d00980",
+    "url": "/react-animations/static/css/main.4086c106.chunk.css"
+  },
+  {
+    "revision": "1eaaab0b6c8a2475f1bf",
+    "url": "/react-animations/static/js/2.33e7d5df.chunk.js"
+  },
+  {
+    "revision": "2527e0d9de1b92d00980",
+    "url": "/react-animations/static/js/main.b4f30e94.chunk.js"
+  },
+  {
+    "revision": "3cf1a1c7d12d2a060d87",
+    "url": "/react-animations/static/js/runtime~main.eb47dc47.js"
+  }
+]);
